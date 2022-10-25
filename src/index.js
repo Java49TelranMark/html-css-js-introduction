0d5e75c20fd3631e2847f218d10768334d149dfa
Mark <markgurevich2015@gmail.com>
2022-10-25 14:04:19 +0300

    25.10.2022

--- src/index.js
+++ src/index.js
@@ -3,7 +3,11 @@ const anchors = document.querySelectorAll(".thumbnails-anchor");
 const detailsImage = document.querySelector(".details-image");
 const detailsTitle = document.querySelector(".details-title");
 const mainClass = document.querySelector(".main-class");
+const detailsContainer = document.querySelector(".details-container");
 const HIDDEN = "hidden";
+const IS_POINT = "is-point"
+var audio = new Audio('audio/sample1.mp3');
+
 function setDetails(anchor) {
     detailsImage.setAttribute('src', anchor.getAttribute('data-details-image'));
     detailsTitle.innerHTML = anchor.getAttribute('data-details-title');
@@ -17,6 +21,11 @@ for(let i = 0; i < anchors.length; i++) {
 }
 function showDetails () {
     mainClass.classList.remove(HIDDEN);
+    detailsContainer.classList.add(IS_POINT);
+    setTimeout(function(){
+        detailsContainer.classList.remove(IS_POINT);
+    },1)
+    audio.play();
 }
 function hideDetails() {
     mainClass.classList.add(HIDDEN);

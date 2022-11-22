d701ca0b3a3b967901993e838a51e280b753a3b4
Mark <markgurevich2015@gmail.com>
2022-11-22 12:14:15 +0200

    22.11.2022

--- apps_clean.js
+++ apps_clean.js
@@ -1,54 +1,39 @@
-//******************Task 1
+const original = "yellow";
+const anagram = "weelly";
 
-// let arS = [11, 2, 3, 4, 5, 6, 7, 10,20,-2,50,23];
+const letter = {};
 
-// console.log(minmax(arS));
+console.log(isAnagram(original, anagram));
 
-// function minmax (array){
-//     let min = array[0];
-//     let max = array[0];
-//     for (let i = 0; i < array.length; i++){
-//          min > array[i] ? min = array[i] : min;
-//          max < array[i] ? max = array[i] : max;
-//     }
-//     let newAr = [min, max];
-//     return newAr;
-// }
+function isAnagram(orig, anag){
+    let origAr = Array.from(orig);
+    let anagAr = Array.from(anag);
 
-//*********************Task 2
+    if (origAr.length < anagAr.length){
+        return "too much letters"
+    }
+    if (origAr.length > anagAr.length){
+        return "too few letters"
+    }
 
-// let array = ["abc", "old_abc", "abc_lmn", "123", "old_lmn", "old_123123"];
+    countLetter(origAr);
 
-// console.log(deleteWithPrefix(array, "abc"));
-
-// function deleteWithPrefix(array, string){
-//     let res = array.filter(str => string >= str);
-//     return res;
-// }
-
-// //*********************Task 3
-
-let arS = [11, 2, 3, 4, 5, 6, 7, 10,20,-2,50,23];
-
-console.log(getSorted(arS));
-
-function getSorted(array){
-    let evenNotSorted = getEvenNumbers(array);
-    let oddNotSorted = getOddNumbers(array);
-    let evenSorted = evenNotSorted.sort((a, b) => a - b);
-    let oddSorted = oddNotSorted.sort((a, b) => b - a);
-    let res = evenSorted.concat(oddSorted);
-    return res;
-}
-
-function getEvenNumbers(array){
-        let res = array.filter(n => n % 2 == 0);
-        return res;
-}
-
-    function getOddNumbers(array){
-        let res = array.filter(n => n % 2 != 0);
-        return res;
-}
-
-
+    for (let i = 0; i < anagAr.length; i++){
+        if (letter[anagAr[i]] == undefined){
+            return "no letter " + anagAr[i] + " in the original word";
+        }
+        letter[anagAr[i]]--;
+        if (letter[anagAr[i]] < 0){
+            return "too much letters " + anagAr[i];
+        }}return "anagram";
+    }
+function countLetter(string) {
+    string.forEach(element => {
+    if (letter[element]) {
+        letter[element]++;
+        } else {
+        letter[element] = 1;
+        }
+        });
+    return letter;
+    }

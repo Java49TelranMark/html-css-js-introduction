3169e13743ceba400235f774485135ab42409d23
Mark <markgurevich2015@gmail.com>
2022-11-26 19:18:15 +0200

    26.11.2022 HW

--- apps.js
+++ apps.js
@@ -131,4 +131,64 @@
 // const strings = ["a","opr","lmn", "abc", "lmn","abc", "lmn", "lmn", "abc", "a"];
 // displayOccurrences(strings);
 
-//////////////////////////////////////////////
+////////////////////////////////////////////// HW
+
+// const original = "yellow";
+// const anagram = "weelly";
+
+// const letter = {};
+
+// console.log(isAnagram(original, anagram));
+
+// function isAnagram(orig, anag){
+//     let origAr = Array.from(orig);
+//     let anagAr = Array.from(anag);
+
+//     if (origAr.length < anagAr.length){
+//         return "too much letters"
+//     }
+//     if (origAr.length > anagAr.length){
+//         return "too few letters"
+//     }
+
+//     countLetter(origAr);
+
+//     for (let i = 0; i < anagAr.length; i++){
+//         if (letter[anagAr[i]] == undefined){
+//             return "no letter " + anagAr[i] + " in the original word";
+//         }
+//         letter[anagAr[i]]--;
+//         if (letter[anagAr[i]] < 0){
+//             return "too much letters " + anagAr[i];
+//         }}return "anagram";
+//     }
+// function countLetter(string) {
+//     string.forEach(element => {
+//     if (letter[element]) {
+//         letter[element]++;
+//         } else {
+//         letter[element] = 1;
+//         }
+//         });
+//     return letter;
+//     }
+
+// //////////////////////////////////// CW employees
+
+// function createEmployee(id, name, birthYear, salary) {
+//     return {id, name, birthYear, salary};
+// }
+// const empl1 = createEmployee(123, "Vasya", 2000, 10000);
+// const empl2 = empl1;
+// console.log(empl1==empl2);
+// function updateSalary(empl1, newSalary) {
+//     empl1 = createEmployee(123, "Vasya", 2000, newSalary)
+// }
+// updateSalary(empl1, 15000);
+// console.log(empl1);
+// function updateSalaryPrimitive(salary, newSalary) {
+//     salary = newSalary;
+// }
+// let salary = 10000;
+// updateSalaryPrimitive(salary, 20000);
+// console.log("salary", salary);
